28ebf22be628729fd9404fb83d5c42896c4bafd9
cgail914 <37086045+cgail914@users.noreply.github.com>
2026-09-11 19:17:53 +0200

    Use HTTPS for footer links

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 <!-- footer START -->
 <div id="footer">
 	<a id="gotop" href="#" onclick="MGJS.goTop();return false;"><?php _e('Top', 'inove'); ?></a>
-	<a id="powered" href="http://wordpress.org/">WordPress</a>
+	<a id="powered" href="https://wordpress.org/">WordPress</a>
 	<div id="copyright">
 		<?php
 			global $wpdb;
@@ -36,7 +36,7 @@
 		?>
 	</div>
 	<div id="themeinfo">
-		<?php printf(__('Theme by <a href="%1$s">NeoEase</a>. Valid <a href="%2$s">XHTML 1.1</a> and <a href="%3$s">CSS 3</a>.', 'inove'), 'http://www.neoease.com/', 'http://validator.w3.org/check?uri=referer', 'http://jigsaw.w3.org/css-validator/check/referer?profile=css3'); ?>
+		<?php printf(__('Theme by <a href="%1$s">NeoEase</a>. Valid <a href="%2$s">XHTML 1.1</a> and <a href="%3$s">CSS 3</a>.', 'inove'), 'https://www.neoease.com/', 'https://validator.w3.org/check?uri=referer', 'https://jigsaw.w3.org/css-validator/check/referer?profile=css3'); ?>
 	</div>
 </div>
 <!-- footer END -->
